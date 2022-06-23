0ef66a01360d35cf5b1eaac4a727e556a2ee14c9
Antonio Olvera <antonioolvera1995@gmail.com>
2022-06-23 12:45:47 +0200

    added basic producer and consumer configuration

--- src/Consumer.ts
+++ src/Consumer.ts
@@ -1,6 +1,28 @@
+import { KafkaConsumer as Kconsumer } from "node-rdkafka"
 export class Consumer {
 
     constructor() {
         console.log("Consumer");
+        this.init();
     }
+
+    init() {
+
+        const consumer = new Kconsumer({
+            'group.id': 'kafka',
+            'metadata.broker.list': 'localhost:9092',
+        }, {});
+
+        consumer.connect();
+
+        consumer
+            .on('ready', function () {
+                consumer.subscribe(['librdtesting-01']);
+                consumer.consume();
+            })
+            .on('data', function (data: any) {
+                console.log(data.value.toString());
+            });
+    }
+
 }
--- src/Producer.ts
+++ src/Producer.ts
@@ -1,6 +1,48 @@
+import { Producer as Kproducer } from "node-rdkafka"
 export class Producer {
 
     constructor() {
         console.log("Producer");
+        this.init();
+    }
+
+    init() {
+
+        const producer = new Kproducer({
+            'client.id': 'kafka',
+            'metadata.broker.list': 'localhost:9092',
+            'compression.codec': 'gzip',
+            'retry.backoff.ms': 200,
+            'message.send.max.retries': 10,
+            'socket.keepalive.enable': true,
+            'queue.buffering.max.messages': 100000,
+            'queue.buffering.max.ms': 1000,
+            'batch.num.messages': 1000000,
+            'dr_cb': true
+        });
+
+        producer.connect();
+
+        producer.on('ready', function () {
+            try {
+                producer.produce(
+                    'topic',
+                    null,
+                    Buffer.from('Awesome message'),
+                    'Stormwind',
+                    Date.now(),
+                );
+            } catch (err) {
+                console.error('A problem occurred when sending our message');
+                console.error(err);
+            }
+        });
+
+        producer.on('event.error', function (err) {
+            console.error('Error from producer');
+            console.error(err);
+        })
+
+        producer.setPollInterval(100);
     }
 }
